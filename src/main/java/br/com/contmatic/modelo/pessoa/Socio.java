72d7bd01e9e4c5603780e7e8cdd38fd37839a0fa
stephano.regina <stephano.regina@contmatic.com.br>
2019-11-19 10:57:42 -0200

    commit treino

--- src/main/java/br/com/contmatic/modelo/pessoa/Socio.java
+++ src/main/java/br/com/contmatic/modelo/pessoa/Socio.java
@@ -1,5 +1,18 @@
 package br.com.contmatic.modelo.pessoa;
 
+import javax.validation.constraints.*;
+
+import br.com.contmatic.utilidades.ConstantesNumericas;
+import br.com.contmatic.utilidades.ExpressoesRegularesRegraNegocio;
+import br.com.contmatic.utilidades.MensagensErro;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class Socio {
 
+    
+    
 }
